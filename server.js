3ed670ed4fe49686a98c2ae96cb800e55635db60
Shota Kato <36836040+katonux@users.noreply.github.com>
2019-04-11 23:13:00 +0900

    update
    update

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
-const express = require("express");
-const app = express();
-app.use(express.static(__dirname + "/dist/sokunomi"));
-app.listen(process.env.PORT || 8080);
 const path = require("path");
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname + "/dist/sokunomi/index.html"));
+const express = require("express");
+
+// サーバをインスタンス化する
+const app = express();
+
+// 以下の設定だけで dist/index.html も返せてはいる
+app.use(express.static(`${__dirname}/dist`));
+
+// ルートへのアクセス時は念のため dist/index.html を確実に返すようにしておく
+app.get("/", (req, res) => {
+  res.sendFile(path.join(`${__dirname}/dist/index.html`));
+});
+
+// サーバ起動
+const server = app.listen(process.env.PORT || 8080, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log(`Listening at http://${host}:${port}`);
 });
